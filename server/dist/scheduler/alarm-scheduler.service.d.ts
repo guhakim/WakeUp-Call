7dbca3fbd2ee9f5cc5da5166b239e97fe924a403
guhakim <kgh3274@gmail.com>
2026-06-28 13:47:24 +0000

    chore: node_modules/dist gitignore 추가

--- server/dist/scheduler/alarm-scheduler.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { AlarmService } from '../alarm/alarm.service';
-import { VoipService } from '../voip/voip.service';
-export declare class AlarmSchedulerService {
-    private readonly alarmService;
-    private readonly voipService;
-    private readonly logger;
-    constructor(alarmService: AlarmService, voipService: VoipService);
-    checkOverdueAlarms(): Promise<void>;
-}
